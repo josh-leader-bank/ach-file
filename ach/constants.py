8e2a436085204a609d42ec930fc9ab41533212e0
josh-leader-bank <josh.kotler@leaderbank.com>
2026-03-31 14:40:00 -0400

    fix enum and company id restriction

--- ach/constants.py
+++ ach/constants.py
@@ -36,11 +36,6 @@ class TransactionCode(enum.IntEnum):
 
     @classmethod
     def _missing_(cls, value):
-        return cls._create_pseudo_member_(value)
-
-    # pylint: disable=attribute-defined-outside-init
-    @classmethod
-    def _create_pseudo_member_(cls, value):
         pseudo_member = cls._value2member_map_.get(value, None)
         if pseudo_member is None:
             new_member = int.__new__(cls, value)
